619c5e798aa51d843f1f0dffa31e360dc8b014cd
Stephen Dunn <dunn.stephen@gmail.com>
2016-03-21 21:31:52 -0400

    more heuristics

--- SemiprimeFactor/src/com/entangledloops/heuristicsearch/semiprime/Heuristic.java
+++ SemiprimeFactor/src/com/entangledloops/heuristicsearch/semiprime/Heuristic.java
@@ -8,18 +8,28 @@ import java.util.function.Function;
  */
 public enum Heuristic
 {
-  DIST_DIFF_BY_LEN("Distribution Difference by Length",
+  DIST_EXPECTED_SEPARATE("Expected Distribution (separate)",
       "Calculate distribution difference from target.\nabs( sum(factor[i].bitCount() / factor[i].bitLength()) - (targetBitCount / targetBitLen) )",
       (n) -> Math.abs((((double)n.p.bitCount() / (double)n.p.bitLength()) + ((double)n.q.bitCount() / (double)n.q.bitLength())) - Solver.cacheSemiprimeBitCountOverBitLen)
   ),
 
-  DIST_DIFF_BY_DEPTH("Distribution Difference by Depth",
-      "Calculate distribution difference from target.\nabs( [ sum(factor[i].bitCount()) / (numFactors * (depth+1)) ] - (targetBitCount / targetBitLen) )",
+  DIST_EXPECTED_SUMMED("Expected Distribution (summed)",
+      "Calculate h based upon the likelihood that the current factor bit distribution reflects\nexpectations based upon objective experimental results w/semiprime numbers.",
+      (n) -> Math.abs(((double)(n.p.bitCount() + n.q.bitCount())/((double)(n.p.bitLength() + n.q.bitLength()))) - Solver.cacheSemiprimeBitCountOverBitLen)
+  ),
+
+  DIST_DIFF_BY_DEPTH_SEPARATE("Distribution Difference by Depth (separate)",
+      "Calculate distribution difference from target.\nabs( [ ((p.bitCount/(1+depth))+(q.bitCount/(1+depth)) ] - (targetBitCount/targetBitLen) )",
+      (n) -> Math.abs( (((double)n.p.bitCount()/(1.0+n.depth)) + ((double)n.q.bitCount()/(1.0+n.depth))) - Solver.cacheSemiprimeBitCountOverBitLen)
+  ),
+
+  DIST_DIFF_BY_DEPTH_SUMMED("Distribution Difference by Depth (summed)",
+      "Calculate distribution difference from target.\nabs( [ sum(factor[i].bitCount) / (2*(depth+1)) ] - (targetBitCount / targetBitLen) )",
       (n) -> Math.abs(((n.p.bitCount() + n.q.bitCount()) / (2.0 * (1.0+n.depth))) - Solver.cacheSemiprimeBitCountOverBitLen)
   ),
 
-  DIST_EXPECTED("Expected Distribution",
-      "Calculate h based upon the likelihood that the current factor bit distribution reflects\nexpectations based upon objective experimental results w/semiprime numbers.",
+  DIST_EXPECTED_GAUSSIAN("Expected Distribution (gaussian)",
+      "Calculate h based upon the likelihood that the current factor bit distribution reflects\nexpectations based upon a standard Gaussian distribution.",
       (n) -> Math.abs(0.5 - (((double)n.p.bitCount()/(double)n.p.bitLength()) + (double)n.q.bitCount()/(double)n.q.bitLength()))
   ),
 
