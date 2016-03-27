0ab2e6921131318a69e22e357df7568affcb658b
Stephen Dunn <dunn.stephen@gmail.com>
2016-03-27 16:49:33 -0400

    consistency

--- SemiprimeFactor/src/com/entangledloops/heuristicsearch/semiprime/Experiments.java
+++ SemiprimeFactor/src/com/entangledloops/heuristicsearch/semiprime/Experiments.java
@@ -18,25 +18,21 @@ public class Experiments
     final Random random = new Random(1);
     final Consumer<Solver.Node> callback = (n) -> Log.o(n.toString());
 
-    for (int i = 10; i < 11; ++i)
+    for (int i = 10; i < 50; ++i)
     {
+      final BigInteger p = BigInteger.probablePrime(i, random);
+      final BigInteger q = BigInteger.probablePrime(i, random);
+      final BigInteger pq = p.multiply(q);
+
       for (Heuristic heuristic : Heuristic.values())
       {
         Solver.reset();
         Solver.callback(callback);
         Solver.heuristics(heuristic);
 
-        final BigInteger p = BigInteger.probablePrime(i, random);
-        final BigInteger q = BigInteger.probablePrime(i, random);
-        final BigInteger pq = p.multiply(q);
-
         final Solver solver1 = new Solver(pq);
-        solver1.start(); solver1.join();
-
-        Solver.pLength(pq.bitLength()/2); Solver.qLength(pq.bitLength()/2);
-
-        final Solver solver2 = new Solver(pq);
-        solver2.start(); solver2.join();
+        solver1.start();
+        solver1.join();
       }
     }
   }
