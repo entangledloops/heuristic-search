fb9e418835414ccc8962e6727ef909e70983a82d
Stephen Dunn <dunn.stephen@gmail.com>
2016-04-13 20:51:09 -0400

    numerous test improvements
    interesting results produced

--- SemiprimeFactor/src/com/entangledloops/heuristicsearch/semiprime/Test.java
+++ SemiprimeFactor/src/com/entangledloops/heuristicsearch/semiprime/Test.java
@@ -1,5 +1,6 @@
 package com.entangledloops.heuristicsearch.semiprime;
 
+import java.io.File;
 import java.io.PrintWriter;
 import java.math.BigInteger;
 import java.text.SimpleDateFormat;
@@ -9,12 +10,12 @@ import java.util.Random;
 /**
  * Created by setem on 3/22/16.
  */
-public class Tests
+public class Test
 {
   private final static int              seed   = 1;
   private final static Random           random = new Random(seed);
   private final static SimpleDateFormat format = new SimpleDateFormat("MM-dd-yyyy");
-  private final static String           prefix = "tests/" + format.format(new Date()) + ".seed-" + seed;
+  private final static String           prefix = "test/" + format.format(new Date()) + ".seed-" + seed;
 
   private static class Key
   {
@@ -28,11 +29,11 @@ public class Tests
     final BigInteger q = BigInteger.probablePrime(len/2, random);
     final BigInteger s = p.multiply(q);
 
-    Log.o(
-        "p\n\n" + p.toString() + "\n\n" + p.toString(2) + "\n\n" +
-        "q\n\n" + q.toString() + "\n\n" + q.toString(2) + "\n\n" +
-        "s\n\n" + s.toString() + "\n\n" + s.toString(2) + "\n\n"
-    );
+    Log.o("[" +
+        "\np\n\n" + p.toString() + "\n\n" + p.toString(2) + "\n" +
+        "\nq\n\n" + q.toString() + "\n\n" + q.toString(2) + "\n" +
+        "\ns\n\n" + s.toString() + "\n\n" + s.toString(2) + "\n" +
+        "]\n");
 
     return new Key(p, q, s);
   }
@@ -81,15 +82,39 @@ public class Tests
       }
   }
 
+  private static int[] runs(BigInteger i)
+  {
+    final int runs[] = new int[ i.bitLength() ];
+    for (int j = 0; j < runs.length; ++j) runs[j] = 0;
+
+    int cur = 0;
+    for (int j = 0; j < runs.length; ++j)
+    {
+      if (i.testBit(j)) ++cur;
+      else if (cur > 0) { ++runs[cur-1]; cur = 0; }
+    }
+    if (cur != 0) ++runs[cur-1];
+
+    return runs;
+  }
+
   public static boolean semiprimes(int len, int repeat)
   {
-    try (final PrintWriter log = new PrintWriter(Tests.prefix + ".sp.log");
-         final PrintWriter csv = new PrintWriter(Tests.prefix + ".sp.csv"))
+    try (final PrintWriter log = new PrintWriter(Test.prefix + ".sp.log");
+         final PrintWriter csv = new PrintWriter(Test.prefix + ".sp.csv"))
     {
-      Log.init(s -> { log.write(s); log.flush(); });
+      Log.init(log::write);
       double pLen = 0, pCount = 0, qLen = 0, qCount = 0, sLen = 0, sCount = 0;
 
-      csv.write("p count,p len,p count/len,q count,q len,q count/len,s count,s len, s count/len\n");
+      for (int i = 0; i < 5; ++i) csv.write("run,p,q,s,");
+      csv.write("\n");
+
+      for (int i = 0; i < 3; ++i) csv.write("count,");
+      for (int i = 0; i < 3; ++i) csv.write("len,");
+      for (int i = 0; i < 3; ++i) csv.write("count/len,");
+      for (int i = 0; i < 3; ++i) csv.write("max run,");
+      for (int i = 0; i < 3; ++i) csv.write("max run count,");
+      csv.write("\n");
 
       final long pRuns[] = new long[len/2]; for (int i = 0; i < pRuns.length; ++i) pRuns[i] = 0;
       final long qRuns[] = new long[len/2]; for (int i = 0; i < qRuns.length; ++i) qRuns[i] = 0;
@@ -97,28 +122,14 @@ public class Tests
 
       for (int i = 0; i < repeat; ++i)
       {
+        Log.o(i + ":\n");
         final Key key = key(len);
 
-        // track number of identical consecutive values in primes and their product
-        int cur = 0;
-        for (int j = 0; j < key.p.bitLength(); ++j)
-        {
-          if (key.p.testBit(j)) ++cur;
-          else if (cur > 0) { ++pRuns[cur-1]; cur = 0; }
-        }
-        if (cur != 0) ++pRuns[cur-1]; cur = 0;
-        for (int j = 0; j < key.q.bitLength(); ++j)
-        {
-          if (key.q.testBit(j)) ++cur;
-          else if (cur > 0) { ++qRuns[cur-1]; cur = 0; }
-        }
-        if (cur != 0) ++qRuns[cur-1]; cur = 0;
-        for (int j = 0; j < key.s.bitLength(); ++j)
-        {
-          if (key.s.testBit(j)) ++cur;
-          else if (cur > 0) { ++sRuns[cur-1]; cur = 0; }
-        }
-        if (cur != 0) ++sRuns[cur-1]; cur = 0;
+        // track number of identical consecutive set bits of each possible length in primes and their product
+        int maxPRunIndex = 1, maxQRunIndex = 1, maxSRunIndex = 1;
+        final int[] curPRuns = runs(key.p); for (int j = 1; j < curPRuns.length; ++j) { pRuns[j] += curPRuns[j]; if (curPRuns[j] > curPRuns[maxPRunIndex]) maxPRunIndex = j; }
+        final int[] curQRuns = runs(key.q); for (int j = 1; j < curQRuns.length; ++j) { qRuns[j] += curQRuns[j]; if (curQRuns[j] > curQRuns[maxQRunIndex]) maxQRunIndex = j; }
+        final int[] curSRuns = runs(key.s); for (int j = 1; j < curSRuns.length; ++j) { sRuns[j] += curSRuns[j]; if (curSRuns[j] > curSRuns[maxSRunIndex]) maxSRunIndex = j; }
 
         // calculate some stats
         final double curPLen = key.p.bitLength(); pLen += curPLen;
@@ -129,17 +140,17 @@ public class Tests
         final double curSCount = key.s.bitCount(); sCount += curSCount;
 
         csv.write(
-            curPCount + "," + curPLen + "," + curPCount/curPLen + "," +
-            curQCount + "," + curQLen + "," + curQCount/curQLen + "," +
-            curSCount + "," + curSLen + "," + curSCount/curSLen + "\n"
+            1+i + "," +
+            curPCount + "," + curQCount + "," + curSCount + "," +
+            curPLen + "," + curQLen + "," + curSLen + "," +
+            curPCount/curPLen + "," + curQCount/curQLen + "," + curSCount/curSLen + "," +
+            1+maxPRunIndex + "," + 1+maxQRunIndex + "," + 1+maxSRunIndex + "," +
+            curPRuns[maxPRunIndex] + "," + curQRuns[maxQRunIndex] + "," + curSRuns[maxSRunIndex] + "\n"
         );
       }
 
-      csv.write("\navgs\n\np,q,s\n" + pCount/pLen + "," + qCount/qLen + "," + sCount/sLen + "\n");
-      csv.write("\np runs\n"); for (int i = 0; i < pRuns.length; ++i) csv.write(1+i + "," + pRuns[i] + "\n");
-      csv.write("\nq runs\n"); for (int i = 0; i < qRuns.length; ++i) csv.write(1+i + "," + qRuns[i] + "\n");
-      csv.write("\ns runs\n"); for (int i = 0; i < sRuns.length; ++i) csv.write(1+i + "," + sRuns[i] + "\n");
-      csv.flush();
+      csv.write("\n,avg\np,q,s\n" + pCount/pLen + "," + qCount/qLen + "," + sCount/sLen + "\n");
+      csv.write("\n\n,sum / run\nrun,p,q,s\n"); for (int i = 1; i < pRuns.length; ++i) csv.write(1+i + "," + pRuns[i] + "," + qRuns[i] + "," + sRuns[i] + "\n");
 
       return true;
     }
@@ -152,7 +163,8 @@ public class Tests
 
   public static void main(String[] args)
   {
-    if (!semiprimes(2048, 100)) System.exit(1);
-    //if (!heuristics(30, 40, 10, Heuristic.values())) System.exit(2);
+    try { new File("test").mkdir(); } catch (Throwable ignored) {}
+    if (!semiprimes(2048, 1000)) System.exit(1);
+    if (!heuristics(30, 40, 10, Heuristic.values())) System.exit(2);
   }
 }
